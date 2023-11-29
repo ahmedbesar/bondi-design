fe8968f9b2e6dba3894aa0dc5285656411ea9237
Ahmed-Besar <ahmedgamalbesar11@gmail.com>
2023-11-29 17:12:44 +0200

    Add Bondi Files

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+// async function fetchdata(resolve,reject)
+// {
+//     console.log("before");
+// try{
+//     let data= await fetch("text.json");
+//     let alldata=await data.json()
+//     console.log( alldata[0]);
+// }
+// catch(reason){
+// console.log(`reason is :  ${reason}`)
+// }finally{
+
+//     console.log("after");
+// }
+// }
+
+// fetchdata();
